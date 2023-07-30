b9ce93f28c18eeddfb9bfb078a6ea768e926edbf
Luiz Henrique <luizhjramos@outlook.com>
2023-07-30 11:36:01 -0300

    feat: apply global error validate on the create user controller

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/MogLuiz/go-person-api/src/configuration/error_logger"
+	"github.com/MogLuiz/go-person-api/src/configuration/validation"
 	"github.com/MogLuiz/go-person-api/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +12,8 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		error := error_logger.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		error := validation.ValidateUserError(err)
+
 		c.JSON(error.Code, error)
 		return
 	}
