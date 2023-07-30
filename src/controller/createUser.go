1cdedfad6f43ea9e6802d3714ae2d478bcc0e68e
Luiz Henrique <luizhjramos@outlook.com>
2023-07-30 00:15:54 -0300

    feat: add UserRequest tag validators

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,21 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/MogLuiz/go-person-api/src/configuration/error_logger"
+	"github.com/MogLuiz/go-person-api/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := error_logger.NewBadRequestError("Teste de erro")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		error := error_logger.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(error.Code, error)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
