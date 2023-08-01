c41e50eb918edccd2a9782c671cab91140e61c11
Luiz Henrique <luizhjramos@outlook.com>
2023-08-01 13:26:24 -0300

    feat: add FindUserByID controller layer

--- controller/findUser.go
+++ controller/findUser.go
@@ -1,7 +1,37 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (uc *userControllerInterface) FindUserByID(c *gin.Context) {}
+	"github.com/MogLuiz/go-person-api/configuration/error_handle"
+	"github.com/MogLuiz/go-person-api/configuration/logger"
+	"github.com/MogLuiz/go-person-api/view"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func (uc *userControllerInterface) FindUserByID(c *gin.Context) {
+	logger.Info("Init FindUserByID controller", logger.AddJourneyTag(logger.FindUserByIDJourney))
+
+	userID := c.Param("userId")
+
+	if _, err := uuid.Parse(userID); err != nil {
+		errorMessage := error_handle.NewBadRequestError("userId is not a valid UUID")
+		logger.Error("Error trying to validate userId", err, logger.AddJourneyTag(logger.FindUserByIDJourney))
+
+		c.JSON(errorMessage.Code, errorMessage)
+		return
+	}
+
+	userDomain, err := uc.service.FindUserByID(userID)
+	if err != nil {
+		logger.Error("Error trying to call findUserByID service", err, logger.AddJourneyTag(logger.FindUserByIDJourney))
+		c.JSON(err.Code, err)
+		return
+	}
+
+	logger.Info("FindUserByID controller executed successfully", logger.AddGenericTag("userID", userDomain.GetID()), logger.AddJourneyTag(logger.FindUserByIDJourney))
+	c.JSON(http.StatusOK, view.ConvertDomainToResponse(userDomain))
+}
 
 func (uc *userControllerInterface) FindUserByEmail(c *gin.Context) {}
