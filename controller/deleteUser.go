8938e3154782e56cb6a0099b3040668f367d3056
Luiz Henrique <luizhjramos@outlook.com>
2023-08-02 21:17:46 -0300

    feat: add deleteUser controller layer

--- controller/deleteUser.go
+++ controller/deleteUser.go
@@ -1,5 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"strings"
 
-func (uc *userControllerInterface) DeleteUser(c *gin.Context) {}
+	"github.com/MogLuiz/go-person-api/configuration/error_handle"
+	"github.com/MogLuiz/go-person-api/configuration/logger"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (uc *userControllerInterface) DeleteUser(c *gin.Context) {
+	logger.Info("Init deleteUser controller", logger.AddJourneyTag(logger.DeleteUserJourney))
+
+	userID := c.Param("userId")
+	if _, err := primitive.ObjectIDFromHex(userID); err != nil || strings.TrimSpace(userID) == "" {
+		errorMessage := error_handle.NewBadRequestError("userId is not a valid UUID")
+		logger.Error("Error trying to validate userId", err, logger.AddJourneyTag(logger.FindUserByIDJourney))
+
+		c.JSON(errorMessage.Code, errorMessage)
+		return
+	}
+
+}
