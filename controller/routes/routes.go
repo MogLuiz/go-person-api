3778a63813a449fff57b5ed0542824b863029668
Luiz Henrique <luizhjramos@outlook.com>
2023-08-06 17:41:16 -0300

    feat: apply jwt token validator middleware

--- controller/routes/routes.go
+++ controller/routes/routes.go
@@ -2,15 +2,16 @@ package routes
 
 import (
 	"github.com/MogLuiz/go-person-api/controller"
+	"github.com/MogLuiz/go-person-api/model"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
-	r.GET("/getUserById/:userId", userController.FindUserByID)
-	r.GET("/getUserByEmail/:userEmail", userController.FindUserByEmail)
-	r.POST("/createUser", userController.CreateUser)
-	r.PUT("/updateUser/:userId", userController.UpdateUser)
-	r.DELETE("/deleteUser/:userId", userController.DeleteUser)
+	r.GET("/getUserById/:userId", model.VerifyTokenMiddleware, userController.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", model.VerifyTokenMiddleware, userController.FindUserByEmail)
+	r.POST("/createUser", model.VerifyTokenMiddleware, userController.CreateUser)
+	r.PUT("/updateUser/:userId", model.VerifyTokenMiddleware, userController.UpdateUser)
+	r.DELETE("/deleteUser/:userId", model.VerifyTokenMiddleware, userController.DeleteUser)
 
 	r.POST("/login", userController.Login)
 }
