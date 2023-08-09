8e591bce7a6ce54061934c92c76965cb8c4b3e29
Luiz Henrique <luizhjramos@outlook.com>
2023-08-08 21:38:07 -0300

    test(unit): ensure returns success when is sended a valid domain

--- model/repository/create_user_test.go
+++ model/repository/create_user_test.go
@@ -3,11 +3,44 @@ package repository
 import (
 	"os"
 	"testing"
+
+	"github.com/MogLuiz/go-person-api/model"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
 
 func TestUserRepository_CreateUser(t *testing.T) {
 	database_name := "user_database_test"
 	collection_name := "user_collection_test"
 
-	os.Setenv("DATABASE_NAME", database_name)
+	os.Setenv("MONGODB_USER_DB", collection_name)
+	defer os.Clearenv()
+
+	mtestDB := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mtestDB.Close()
+
+	mtestDB.Run("it should returns success when is sended a valid domain", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 1},
+			{Key: "n", Value: 1},
+			{Key: "acknowledged", Value: true},
+		})
+		databaseMock := mt.Client.Database(database_name)
+
+		repository := NewUserRepository(databaseMock)
+		domain := model.NewUserDomain("test@test.com", "test", "test", 90)
+		userDomain, err := repository.CreateUser(domain)
+
+		_, errID := primitive.ObjectIDFromHex(userDomain.GetID())
+
+		assert.Nil(t, err)
+		assert.Nil(t, errID)
+
+		assert.EqualValues(t, userDomain.GetEmail(), domain.GetEmail())
+		assert.EqualValues(t, userDomain.GetPassword(), domain.GetPassword())
+		assert.EqualValues(t, userDomain.GetName(), domain.GetName())
+		assert.EqualValues(t, userDomain.GetAge(), domain.GetAge())
+	})
 }
