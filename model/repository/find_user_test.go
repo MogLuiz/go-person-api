9bf0a78f72870ed88ce9533885d1b4e953b1dfe5
Luiz Henrique <luizhjramos@outlook.com>
2023-08-18 19:55:09 -0300

    test(unit): ensure returns success when is sended a valid id

--- model/repository/find_user_test.go
+++ model/repository/find_user_test.go
@@ -87,6 +87,47 @@ func TestUserRepository_FindUserByEmail(t *testing.T) {
 	})
 }
 
+func TestUserRepository_FindUserByID(t *testing.T) {
+	database_name := "user_database_test"
+	collection_name := "user_collection_test"
+
+	os.Setenv("MONGODB_USER_DB", collection_name)
+	defer os.Clearenv()
+
+	mtestDB := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mtestDB.Close()
+
+	mtestDB.Run("it should returns success when is sended a valid id", func(mt *mtest.T) {
+		userEntity := entity.UserEntity{
+			ID:       primitive.NewObjectID(),
+			Email:    "test@test.com",
+			Password: "test",
+			Name:     "test",
+			Age:      50,
+		}
+
+		mt.AddMockResponses(mtest.CreateCursorResponse(
+			1,
+			fmt.Sprintf("%s.%s", database_name, collection_name),
+			mtest.FirstBatch,
+			convertEntityToBsonD(userEntity)))
+
+		databaseMock := mt.Client.Database(database_name)
+
+		repository := NewUserRepository(databaseMock)
+		userDomain, err := repository.FindUserByID(string(userEntity.ID.Hex()))
+
+		assert.Nil(t, err)
+
+		assert.EqualValues(t, userDomain.GetID(), userEntity.ID.Hex())
+		assert.EqualValues(t, userDomain.GetEmail(), userEntity.Email)
+		assert.EqualValues(t, userDomain.GetPassword(), userEntity.Password)
+		assert.EqualValues(t, userDomain.GetName(), userEntity.Name)
+		assert.EqualValues(t, userDomain.GetAge(), userEntity.Age)
+	})
+
+}
+
 func convertEntityToBsonD(entity entity.UserEntity) bson.D {
 	return bson.D{
 		{Key: "_id", Value: entity.ID},
