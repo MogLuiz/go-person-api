670727cd1ba3ebb8b487f212097ce342b9833ad3
Luiz Henrique <luizhjramos@outlook.com>
2023-10-17 21:06:34 -0300

    refactor: find user service exception responsability

--- model/services/find_user.go
+++ model/services/find_user.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"net/http"
-
 	"github.com/MogLuiz/go-person-api/configuration/error_handle"
 	"github.com/MogLuiz/go-person-api/configuration/logger"
 	"github.com/MogLuiz/go-person-api/model"
@@ -11,33 +9,14 @@ import (
 func (ud *userDomainService) FindUserByID(id string) (model.UserDomainInterface, *error_handle.ErrorHandle) {
 	logger.Info("Init findUserByID service", logger.AddJourneyTag(logger.FindUserByIDJourney))
 
-	userDomainRepository, err := ud.repository.FindUserByID(id)
-	if err != nil {
-		if err.Code == http.StatusNotFound {
-			return nil, err
-		}
-
-		logger.Error("Error trying to call findUserByID repository", err, logger.AddJourneyTag(logger.FindUserByIDJourney))
-		return nil, error_handle.NewInternalServerError(err.Error())
-	}
-
-	return userDomainRepository, nil
+	return ud.repository.FindUserByID(id)
 }
 
 func (ud *userDomainService) FindUserByEmail(email string) (model.UserDomainInterface, *error_handle.ErrorHandle) {
 	logger.Info("Init findUserByEmail service", logger.AddJourneyTag(logger.FindUserByEmailJourney))
 
-	userDomainRepository, err := ud.repository.FindUserByEmail(email)
-	if err != nil {
-		if err.Code == http.StatusNotFound {
-			return nil, err
-		}
+	return ud.repository.FindUserByEmail(email)
 
-		logger.Error("Error trying to call findUserByEmail repository", err, logger.AddJourneyTag(logger.FindUserByEmailJourney))
-		return nil, error_handle.NewInternalServerError(err.Error())
-	}
-
-	return userDomainRepository, nil
 }
 
 func (ud *userDomainService) findUserByEmailAndPassword(email, password string) (model.UserDomainInterface, *error_handle.ErrorHandle) {
