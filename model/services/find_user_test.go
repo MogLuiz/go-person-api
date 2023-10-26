275298dea148c3a071e27f7a921759e736523e4f
Luiz Henrique <luizhjramos@outlook.com>
2023-10-25 22:58:18 -0300

    test: ensure returns success when exists user

--- model/services/find_user_test.go
+++ model/services/find_user_test.go
@@ -3,49 +3,30 @@ package services
 import (
 	"testing"
 
-	"github.com/MogLuiz/go-person-api/configuration/error_handle"
 	"github.com/MogLuiz/go-person-api/model"
 	"github.com/MogLuiz/go-person-api/test/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/mock/gomock"
 )
 
-func TestFindUserByIDService(t *testing.T) {
+func TestUserDomainService_FindUserByIDService(t *testing.T) {
 	control := gomock.NewController(t)
 	defer control.Finish()
-
-	id := "123"
 
 	repository := mocks.NewMockUserRepository(control)
 	service := NewUserDomainService(repository)
 
-	repository.EXPECT().FindUserByID(id).Return(model.NewUserDomain("test@test.com", "123", "john doe", 21), nil)
+	t.Run("it should return success when exists user", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+		userDomain := model.NewUserDomain("test@test.com", "123", "john doe", 21)
 
-	user, err := service.FindUserByID(id)
+		repository.EXPECT().FindUserByID(id).Return(userDomain, nil)
 
-	if err != nil {
-		t.Errorf("Error should be nil, but got %v", err)
-	}
-	if user == nil {
-		t.Errorf("User should not be nil")
-	}
-	if user.GetEmail() != "test@test.com" {
-		t.Errorf("User email should be test@test.com but got %v", user.GetEmail())
-	}
+		returnedUser, err := service.FindUserByID(id)
 
-	repository2 := mocks.NewMockUserRepository(control)
-	service2 := NewUserDomainService(repository2)
+		assert.Nil(t, err)
+		assert.Equal(t, userDomain, returnedUser)
+	})
 
-	repository2.EXPECT().FindUserByID(id).Return(nil, error_handle.NewNotFoundError("User not found"))
-
-	user2, err2 := service2.FindUserByID(id)
-
-	if err2 == nil {
-		t.Errorf("Error should not be nil")
-	}
-	if user2 != nil {
-		t.Errorf("User should be nil")
-	}
-	if err2.Message != "User not found" {
-		t.Errorf("Error message should be User not found but got %v", err2.Message)
-	}
 }
